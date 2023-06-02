83168cda92617899d1020e0c1683722382fced05
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-02 10:24:14 -0700

    pre-commit

--- tests/matrix.py
+++ tests/matrix.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
 from pathlib import Path
 from time import time
 
-import numpy as np
 import cvxpy as cvx
+import numpy as np
 import pandas as pd
 
 from tests.vector import State as StateVector
@@ -15,7 +18,7 @@ class State:
         self.s = cvx.Variable((self.shape[1], num), name="s")
         self.P_max = P_max or 10
         # self._grad = np.zeros((num,self.shape[0]))
-        self._grad = cvx.Parameter((num,self.shape[0]))
+        self._grad = cvx.Parameter((num, self.shape[0]))
 
     @property
     def shape(self):
@@ -29,7 +32,10 @@ class State:
         # self.update_grad(self.price_centered.value.T)
 
         objective = cvx.Maximize(cvx.trace(self._grad @ self.price_centered))
-        constraints = [cvx.abs(self.price_centered) <= 1.0, self.prices @ cvx.abs(self.s) <= self.P_max]
+        constraints = [
+            cvx.abs(self.price_centered) <= 1.0,
+            self.prices @ cvx.abs(self.s) <= self.P_max,
+        ]
         prob = cvx.Problem(objective, constraints)
 
         for _ in range(num):
@@ -56,7 +62,7 @@ class State:
         self._grad.value = 2 * self._grad.value
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file = Path(__file__).parent / "resources" / "price.csv"
 
     # period for training
--- tests/sandbox.py
+++ tests/sandbox.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
 from pathlib import Path
 
-import pandas as pd
-import numpy as np
 import cvxpy as cvx
+import numpy as np
+import pandas as pd
 
 
 class State:
     def __init__(self, prices, spread_max=1, P_max=None):
-        n,m = prices.shape
+        n, m = prices.shape
         self.mu = cvx.Variable(name="mu", nonneg=True)
         self.s = cvx.Variable(m, name="s")
         self.P_max = P_max or 10
@@ -18,7 +21,6 @@ class State:
     def shape(self):
         return self.prices.shape
 
-
     def iteration(self):
         p_centered = self.prices @ self.s - self.mu
         grad = State._get_grad_g2(p_centered.value)
@@ -26,7 +28,13 @@ class State:
         objective = cvx.Maximize(grad @ p_centered)
 
         # TODO: I think constraints are wrong; it should be self.P_bar @ cvx.abs(self.s) not self.prices @ cvx.abs(self.s) ?
-        prob = cvx.Problem(objective, constraints=[cvx.abs(p_centered) <= self.spread_max, self.prices @ cvx.abs(self.s) <= self.P_max])
+        prob = cvx.Problem(
+            objective,
+            constraints=[
+                cvx.abs(p_centered) <= self.spread_max,
+                self.prices @ cvx.abs(self.s) <= self.P_max,
+            ],
+        )
         prob.solve(cvx.ECOS)
         return self
 
@@ -38,18 +46,19 @@ class State:
         returns the gradient of g at pk
         """
         grad_g = np.zeros(pk.shape)
-        grad_g[0] = pk[0]-pk[1]
-        grad_g[-1] = pk[-1]-pk[-2]
-        grad_g[1:-1] = 2*pk[1:-1] - pk[:-2] - pk[2:]
+        grad_g[0] = pk[0] - pk[1]
+        grad_g[-1] = pk[-1] - pk[-2]
+        grad_g[1:-1] = 2 * pk[1:-1] - pk[:-2] - pk[2:]
 
-        return 2*grad_g
+        return 2 * grad_g
 
     def build(self):
         mu = self.mu.value
         s = self.s.value
-        #return StatArb(mu, s)
+        # return StatArb(mu, s)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     file = Path(__file__).parent / "resources" / "price.csv"
 
     # period for training
@@ -70,7 +79,7 @@ if __name__ == '__main__':
         state.s.value = np.random.randn(state.shape[1])
 
         # do a single iteration
-        #state = state.iteration()
+        # state = state.iteration()
 
         # do like 20 iterations
         for i in range(20):
@@ -82,11 +91,9 @@ if __name__ == '__main__':
         # x is some sort of statarb
         x = state.build()
 
-        #results = list()
-        if x.validate(some prices):
-            #profit = x.test(some other prices)
+        # results = list()
+        if x.validate(prices):
+            # profit = x.test(some other prices)
             results.append(x)
 
     # results is a list of validated stat arbs
-
-
